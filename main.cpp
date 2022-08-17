467d00bff8595bac9eef0a6d50defb84c9eab461
Rodrigo Perez Millan <perezmillan.r@gmail.com>
2022-08-17 00:43:10 -0300

    Change main

--- main.cpp
+++ main.cpp
@@ -1,48 +1,34 @@
 #include "mbed.h"
 
+// Maximum number of element the application buffer can contain
+#define MAXIMUM_BUFFER_SIZE                                                  32
+
 // Create a DigitalOutput object to toggle an LED whenever data is received.
-static  DigitalOut led1(LED1);
-static  DigitalOut led2(LED2);
-static  DigitalOut led3(LED3);
+static DigitalOut led(LED1);
 
-// Create a UnbufferedSerial object with a default baud rate.
-static UnbufferedSerial serial_port(USBTX, USBRX);
-
-void on_rx_interrupt()
-{
-    char c;
-    // Read the data to clear the receive interrupt.
-    if (serial_port.read(&c, 1)) {
-        // Echo the input back to the terminal.
-        serial_port.write(&c, 1);
-
-        switch(c){
-            case '1':
-                led1 = !led1;
-                break;
-            case '2':
-                led2 = !led2;
-                break;
-            case '3':
-                led3 = !led3;
-                break;
-            default:
-                break;
-        }
-
-    }
-}
+// Create a BufferedSerial object with a default baud rate.
+static BufferedSerial serial_port(USBTX, USBRX);
 
 int main(void)
 {
     // Set desired properties (9600-8-N-1).
-    serial_port.baud(9600);
-    serial_port.format(
+    serial_port.set_baud(9600);
+    serial_port.set_format(
         /* bits */ 8,
-        /* parity */ SerialBase::None,
+        /* parity */ BufferedSerial::None,
         /* stop bit */ 1
     );
 
-    // Register a callback to process a Rx (receive) interrupt.
-    serial_port.attach(&on_rx_interrupt, SerialBase::RxIrq);
+    // Application buffer to receive the data
+    char buf[MAXIMUM_BUFFER_SIZE] = {0};
+
+    while (1) {
+        if (uint32_t num = serial_port.read(buf, sizeof(buf))) {
+            // Toggle the LED.
+            led = !led;
+
+            // Echo the input back to the terminal.
+            serial_port.write(buf, num);
+        }
+    }
 }
